9b2fee3cfc82316001b2b999d7f04e09e26e6c76
TonySMK <tonysun25light@live.ca>
2024-05-21 15:01:41 -0400

    Made test endpoint for accessing veiw

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require("cors");
-const RecieptRoutes = require("./routes/Recipt_summary_route")
-const customersRoutes = require("./routes/customers-routes")
-const menuRoutes = require("./routes/menu-routes")
-const orderRoutes = require("./routes/order-routes")
-const reviewRoutes = require("./routes/review-routes")
+const RecieptRoutes = require("./routes/Recipt_summary_route");
+const customersRoutes = require("./routes/customers-routes");
+const menuRoutes = require("./routes/menu-routes");
+const orderRoutes = require("./routes/order-routes");
+const reviewRoutes = require("./routes/review-routes");
 // const path = require('path');
 const userPage = require("./routes/userpage_route");
 const paymentPage = require("./routes/paymentpage_route");
+const knex = require("knex")(require("./knexfile"));
 
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log("Link validation middleware");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Link validation middleware");
+//   next();
+// });
 
 app.use("/Reciept", RecieptRoutes);
 
@@ -29,10 +30,16 @@ app.get("/db", (req, res) => {
   res.send("Database testing. Huzzah!");
 });
 
-app.use('/customers', customersRoutes);
-app.use('/menu', menuRoutes);
-app.use('/order', orderRoutes);
-app.use('/review', reviewRoutes);
+app.use("/customers", customersRoutes);
+app.use("/menu", menuRoutes);
+app.use("/order", orderRoutes);
+app.use("/review", reviewRoutes);
+
+app.get("/veiw", (req, res) => {
+  knex("user_total_spent")
+    .select("*")
+    .then((data) => res.json(data));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
